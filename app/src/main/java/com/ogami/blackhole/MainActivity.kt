474162964242755e19b88a0036c5885044faf2d2
ogami <ogami@163.com>
2019-04-12 19:33:13 +0800

    第一次提交

---
+++ app/src/main/java/com/ogami/blackhole/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.ogami.blackhole
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ *  第一个 个人App 刚好发布第一张黑洞照片 所以取名叫黑洞
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
